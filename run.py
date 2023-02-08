fb9d0c45b5081136f64ce6b9cb2da728d6e9f25c
Amy Richardson <amyloumc@gmail.com>
2023-02-08 13:37:10 +0000

    Testing input commands

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+#Character details
+races = ['Human', 'Elf', 'Dwarf', 'Orc', 'Goblin', 'Cyborg', 'Werewolf', 'Vampire', 'Giant', 'Dragonborn']
+race = random.choice(races)
+   
+# Start of program
+print("Welcome to Fantasy RPG Character Generator!")
+new_input = input("Create a new character? Please type 'Yes' or 'No'.\n")
+if new_input == "Yes" or new_input == "yes":
+    print("Okay! Let's make a new character!")
+elif new_input =="No" or new_input == "no":
+    print("That's a shame, maybe next time?")
+else:
+    print("That's not an answer I was expecting, please start again.")
+
+
