a40fca6e11e67c9877871ee250d982589a884af5
Amy Richardson <amyloumc@gmail.com>
2023-02-11 21:18:47 +0000

    chore: tidied code

--- run.py
+++ run.py
@@ -1,20 +1,24 @@
 import random
 
 
+# Character details
+races = ['Human', 'human', 'Elf', 'elf', 'Dwarf', 'dwarf', 'Orc', 'orc',
          'Goblin', 'goblin', 'Cyborg', 'cyborg', 'Werewolf', 'werewolf',
+         'Vampire', 'vampire', 'Giant', 'giant', 'Dragonborn', 'dragonborn']
+
 race = random.choice(races)
 
-char_classes = ['Barbarian','Cleric','Druid','Fighter', 'Ranger', 'Wizard', 'Rogue']
+char_classes = ['Barbarian', 'Cleric', 'Druid', 'Fighter',
+                'Ranger', 'Wizard', 'Rogue']
 
 
-  
 # Start of program
 def start_program():
     print("Welcome to Fantasy RPG Character Generator!\n")
     new_input = input("Create a new character? Please type 'Yes' or 'No'.\n")
     if new_input == "Yes" or new_input == "yes":
         print("Okay! Let's make a new character!")
-    elif new_input =="No" or new_input == "no":
+    elif new_input == "No" or new_input == "no":
         print("That's a shame, maybe next time?")
         start_program()
     else:
@@ -23,6 +27,7 @@ def start_program():
 
 
 start_program()
+
 
 def create_new_char():
     print("""
@@ -49,7 +54,7 @@ else:
     elif user_input_race not in races:
         print("""
             That's a new one! Never heard of that!
-            But do please choose from the list... 
+            But do please choose from the list...
             """)
         create_new_char()
 
@@ -60,12 +65,12 @@ def assign_random_name():
     num_vowels = random.randint(2, 3)
     num_cons = random.randint(2, 3)
     name = ''.join([random.choice(vowels) for i in range(num_vowels)] +
-                   [random.choice(cons) for i in range(num_cons)]) 
-    return name.capitalize() 
-    
-    
+                   [random.choice(cons) for i in range(num_cons)])
+    return name.capitalize()
+
+
 new_name = assign_random_name()
-print("Let me bestow upon you a new name! Welcome:", new_name,"!\n")
+print("Let me bestow upon you a new name! Welcome:", new_name, "!\n")
 
 
 print(f"Ok {new_name}, let me choose a class for you...\n")
@@ -74,6 +79,5 @@ print("Pondering...\n")
 char_class = random.choice(char_classes)
 print("Class:", char_class)
 
-print(f"Ah... a {char_class}", user_input_race.capitalize(), "... I haven't seen one of those before.")
-
-
+print(f"Ah... a {char_class}", user_input_race.capitalize(),
+      "... I haven't seen one of those before.")
