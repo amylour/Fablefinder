da37a12ff55dd9f127efd191a721b988f9ead7ec
Amy Richardson <amyloumc@gmail.com>
2023-02-11 15:10:37 +0000

    Created start_program function

--- run.py
+++ run.py
@@ -6,13 +6,5 @@ races = ['Human', 'Elf', 'Dwarf', 'Orc', 'Goblin', 'Cyborg', 'Werewolf', 'Vampir
 race = random.choice(races)
    
 # Start of program
-print("Welcome to Fantasy RPG Character Generator!")
-new_input = input("Create a new character? Please type 'Yes' or 'No'.\n")
-if new_input == "Yes" or new_input == "yes":
-    print("Okay! Let's make a new character!")
-elif new_input =="No" or new_input == "no":
-    print("That's a shame, maybe next time?")
-else:
-    print("That's not an answer I was expecting, please start again.")
-
+    elif new_input =="No" or new_input == "no":
 
